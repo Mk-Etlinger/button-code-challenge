b359069a2594b0159ca23b3320417ee3e7f98654
Mike Etlinger <mk-etlinger@gmail.com>
2018-03-03 15:49:43 -0500

    update next statement to be clearer, remove recursive attempt

--- expression_evaluator.rb
+++ expression_evaluator.rb
@@ -1,9 +1,10 @@
 class ExpressionEvaluator
   # Brute Force attempt, passing all tests, but needs to be more efficient and readable
   def self.parse(string)
+    sum = 0
     char_array = string.split
     char_array.each_with_index do |char, i|
-      next unless char.length < 2 && char.match(/\D/) && char_array[i + 2]
+      next unless char == '-' && char_array[i + 2]
       if char_array[i + 2].to_i == 0
         char_array[i + 1] = char + char_array[i + 1]
       else
