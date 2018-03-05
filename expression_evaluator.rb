a036e83fb8a03ebea5cdb5099fef05b8b77f3773
Mike Etlinger <mk-etlinger@gmail.com>
2018-03-05 00:41:03 -0500

    change to use reduce as it reads better

--- expression_evaluator.rb
+++ expression_evaluator.rb
@@ -6,11 +6,11 @@ class ExpressionEvaluator
   end 
 
   def self.complex_sum(array)
-    array.inject(0) { |sum, exp| sum + (exp[2].to_i - exp[4].to_i) }
+    array.reduce(0) { |sum, exp| sum + (exp[2].to_i - exp[4].to_i) }
   end
     
   def self.simple_sum(string)
     return 0 if string.empty?
-    string.gsub('- ', '-').split.inject(0) { |sum, num| sum + num.to_i }
+    string.gsub('- ', '-').split.reduce(0) { |sum, num| sum + num.to_i }
   end
 end
