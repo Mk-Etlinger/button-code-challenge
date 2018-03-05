8c6ee41e17764a7480d0892c44c6306076ba9cd5
Mike Etlinger <mk-etlinger@gmail.com>
2018-03-04 20:19:35 -0500

    update method names, use inject with complex_sum, remove comments

--- expression_evaluator.rb
+++ expression_evaluator.rb
@@ -1,25 +1,16 @@
 class ExpressionEvaluator
-  # Regex implementation
   def self.parse(string)
-    # separate the complex exps from the simple
     complex_exps = string.scan(/-\s\d\s\d/)
     simple_exps = string.gsub(/-\s\d\s\d/, '')
-    # Add the sum of the complex exps to the sum of the simple exps
-    sum_complex_exp(complex_exps) + sum_simple_exp(simple_exps)
+    complex_sum(complex_exps) + simple_sum(simple_exps)
   end 
 
-  def self.sum_complex_exp(array)
-    sum = 0
-    array.each do |exp|
-      sum += (exp[2].to_i - exp[4].to_i)
-    end
-    sum
+  def self.complex_sum(array)
+    array.inject(0) { |sum, exp| sum + (exp[2].to_i - exp[4].to_i) }
   end
     
-  def self.sum_simple_exp(string)
-    # string is empty if there's no simple exps
+  def self.simple_sum(string)
     return 0 if string.empty?
-    # remove spaces between - and split to array. Then convert to int and reduce
     string.gsub('- ', '-').split.map(&:to_i).reduce(&:+)
   end
 end
