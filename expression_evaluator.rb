7017bb36497fe3d1678faab1cf2c86caa391f944
Mike Etlinger <mk-etlinger@gmail.com>
2018-03-08 10:03:36 -0500

    update regex to handle multi digit numbers, add split to complex sum reduce

--- expression_evaluator.rb
+++ expression_evaluator.rb
@@ -1,12 +1,13 @@
+require 'pry'
 class ExpressionEvaluator
   def self.parse(string)
-    complex_exps = string.scan(/-\s\d\s\d/)
-    simple_exps = string.gsub(/-\s\d\s\d/, '')
+    complex_exps = string.scan(/-\s\d+\s\d+/)
+    simple_exps = string.gsub(/-\s\d+\s\d+/, '')
     complex_sum(complex_exps) + simple_sum(simple_exps)
   end 
 
   def self.complex_sum(array)
-    array.reduce(0) { |sum, exp| sum + (exp[2].to_i - exp[4].to_i) }
+    array.reduce(0) { |sum, exp| sum + (exp.split[1].to_i - exp.split[2].to_i) }
   end
     
   def self.simple_sum(string)
